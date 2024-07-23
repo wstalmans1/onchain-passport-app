bfc376e4717020983ad76f2d9b3666531b207ccc
Wim Stalmans <wim.stalmans@gmail.com>
2024-07-23 15:34:13 +0200

    adjusted the ABI import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
 import { ChakraProvider, Flex, Heading, Button } from '@chakra-ui/react'
 import { TabLayout } from './tab-contents'
-import { GITCOIN_PASSPORT_WEIGHTS } from './stamp-weights';
+import { DecoderAbi } from './PassportDecoderABI';
+
+
+//import { GITCOIN_PASSPORT_WEIGHTS } from './stamp-weights';
  
 const decoderContractAddress = "0x5558D441779Eca04A329BcD6b47830D2C6607769";
-const abi = require('./PassportDecoderABI.json')
+//const abi = require('./PassportDecoderABI.ts')
  
 declare global {
   interface Window {
@@ -29,7 +32,7 @@ export default function Passport() {
   const [connected, setConnected] = useState<boolean>(false)
   const [hasStamps, setHasStamps] = useState<boolean>(false)
   const [stamps, setStamps] = useState<Array<Stamp>>([])
-  const [score, setScore] = useState<Number>(0)
+  const [score, setScore] = useState<number>(0)
   const [network, setNetwork] = useState<string>('')
  
   useEffect(() => {
@@ -57,6 +60,50 @@ export default function Passport() {
     }
     return true
   }
+
+  /** get passport info from decoder contract */
+  async function getPassportInfo() {
+    console.log(address)
+    const decoderContract: ethers.Contract = new ethers.Contract(decoderContractAddress, new ethers.Interface(DecoderAbi['0x1a4']), provider)
+    try {
+      const passportInfo: [] = await decoderContract.getPassport(address)
+      return passportInfo
+    } catch {
+      throw new Error("no passport information available")
+    }
+  }
+
+  /** parse out stamps from passport info object*/
+  function getStamps(passportInfo: []) {
+    var stamps: Stamp[] = [];
+    for (var i = 0; i < passportInfo.length; i++) {
+      stamps.push({ id: i, stamp: passportInfo[i][0] })
+    }
+    return stamps
+  }
+
+  /** get poassport score from decoder contract */
+  async function getScore() {
+    const decoderContract: ethers.Contract = new ethers.Contract(decoderContractAddress, new ethers.Interface(DecoderAbi['0x1a4']), provider)
+    try {
+      const score = await decoderContract.getScore(address)
+      return score
+    } catch {
+      throw new Error("no passport info available")
+    }
+  }
+
+  /** call getPassportInfo and getStamps and set state vars */
+  async function queryPassport() {
+    const passportData = await getPassportInfo();
+    const stamps = getStamps(passportData);
+    if (stamps.length > 1) {
+      setHasStamps(true)
+      setStamps(stamps)
+    }
+    const score = await getScore()
+    setScore(parseInt(score) / 10000)
+  }
  
   const styles = {
     main: {
@@ -76,8 +123,8 @@ export default function Passport() {
         </Flex>
         <div>
           {connected && <p>✅ Wallet connected</p>}
-          {connected && network == "10" && <p>✅ network: Optimism Sepolia</p>}
-          {connected && network != "10" && <p>🔴 Please switch to Optimism Sepolia network</p>}
+          {connected && network == "10" && <p>✅ network: Optimism</p>}
+          {connected && network != "10" && <p>🔴 Please switch to Optimism network</p>}
         </div>
         <br />
         <br />
